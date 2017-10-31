c953c285c1efde599627931b971254b444113774
Abe Rudy <aberudy@gmail.com>
2017-10-31 17:43:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,22 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    set :views, Proc.new { File.join('views', 'pirates') }
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params["pirate"]) #working
+      @ships = params["pirate"]["ships"].map {|ship| Ship.new(ship)}
+      # binding.pry
+      erb :show
+    end
 
   end
 end
